5ab6a4a3a296f2a75e65873a32b51865a222dba8
zhangshupu <503032527@qq.com>
2018-10-17 15:10:56 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import '@/styles/common.css';
 Vue.config.productionTip = false
 Vue.config.productionTip=false;
 /* eslint-disable no-new */
